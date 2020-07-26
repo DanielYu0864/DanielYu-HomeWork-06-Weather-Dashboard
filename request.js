1f4dd97221dfb0456548af4f6760b7bc7bc075b6
Daniel.Yu <adam741963@gmail.com>
2020-07-26 14:35:44 -0700

    ajax and fatch

--- request.js
+++ request.js
@@ -1,25 +1,23 @@
-// const apiKey = "e93223a6b1823d41860077c8e54b5206";
+// api key
+const apiKey = "e93223a6b1823d41860077c8e54b5206";
 
-// const baseWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=seattle&appid=e93223a6b1823d41860077c8e54b5206"
-// https://api.openweathermap.org/data/2.5/forecast?q=seattle&appid=e93223a6b1823d41860077c8e54b5206
-
-// const requestCity = async (cityName) => {
-//     const baseWeatherURL = "http://api.openweathermap.org/data/2.5/weather";
-//     const query = `?q=${cityName}&appid=${apiKey}`;
-//     // make fetch call (promise call)
-//     const response = await fetch(baseWeatherURL + query);
-//     // promise data
-//     const data = await response.json();
-//     return data;
-// }
-
-
-// const forecastCity = async (cityName) => {
-//     const baseForcastURL = "https://api.openweathermap.org/data/2.5/forecast";
-//     const query = `?id=${cityName}&appid=${apiKey}`;
-//     // make fetch call (promise call)
-//     const response = await fetch(baseForcastURL + query);
-//     // promise data
-//     const data = await response.json();
-//     return data;
-// }
+// function to fetch current weather URL
+const requestCity = async (cityName) => {
+    const baseWeatherURL = "https://api.openweathermap.org/data/2.5/weather";
+    const query = `?q=${cityName}&appid=${apiKey}`;
+    // make fetch call (promise call)
+    const response = await fetch((baseWeatherURL + query), { method: "GET" });
+    // promise data
+    const data = await response.json();
+    return data;
+}
+// function to fetch forcast URL
+const requestForecastCity = async (cityid) => {
+    const queryforecastURL = "https://api.openweathermap.org/data/2.5/forecast";
+    const query = `?id=${cityid}&appid=${apiKey}`;
+    // make fetch call (promise call)
+    const response = await fetch((queryforecastURL + query), {method: "GET"});
+    // promise data
+    const data = await response.json();
+    return data;
+}
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const apiKey = "e93223a6b1823d41860077c8e54b5206";
+// const apiKey = "e93223a6b1823d41860077c8e54b5206";
 
+// giving the variable
 const searchForm = document.querySelector(".search-loaction");
 const cityValue = document.querySelector(".search-loaction input");
 const cityName = document.querySelector(".city-name p");
@@ -12,61 +13,43 @@ let citys = [];
 let city="";
 
 
-function addToList(c) {
-    let listEl = $("<button>"+ c +"</button>");
-    $(listEl).addClass("list-group-item shadow-sm");
-    $(listEl).attr("data-value",c);
-    $(".list-group").append(listEl);
-}
-
-function loadlastCity(){
-    $("ul").empty();
-    let citysList = JSON.parse(localStorage.getItem("cityName"));
-    console.log(citysList);
-    if(citysList!==null){
-        for(i=0; i<citysList.length;i++){
-            addToList(citysList[i]);
-        }
-    }
-}
-
-
+// turn kelvin to celcius
 function spitOutCelcius(kelvin) {
     celcius = Math.round(kelvin - 273.15);
     return celcius;
 }
-
+// check time to display background
 function isDayTime(icon) {
     if (icon.includes("d")) { return true }
     else { return false }
 }
 
-function forecast(cityid){
-    const queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id=" + cityid + "&appid=" + apiKey;
-    $.ajax({
-        url:queryforcastURL,
-        method:"GET"
-    }).then(function(response){
 
-        for (i=0;i<5;i++){
-            let date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
-            let iconcode= response.list[((i+1)*8)-1].weather[0].icon;
-            let iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
-            let tempK= response.list[((i+1)*8)-1].main.temp;
-            let tempC=spitOutCelcius(tempK).toFixed(0);
-            let humidity= response.list[((i+1)*8)-1].main.humidity;
+// function to get input value and call the display function
+function searchCity(event) {
+    event.preventDefault();
+    const citySearched = cityValue.value.trim();
+    console.log(citySearched);
+    searchForm.reset();
+    // *fetch version
+    requestCity(citySearched).then((data) => {
+        undateWeatherApp(data);
+    }).catch((error) => {
+        console.log(error);
+    })
 
-            $("#fDate"+i).html(date);
-            $("#fImg"+i).html("<img src="+iconurl+">");
-            $("#fTemp"+i).html(` ${tempC}&deg;C`);
-            $("#fHumidity"+i).html(` ${humidity}%`);
-            cardFInfo.classList.remove("display-none");
-        }
+    // *.ajax version
+    // $.ajax({
+    //     url:`https://api.openweathermap.org/data/2.5/weather?q=${citySearched}&appid=${apiKey}`,
+    //     method: "GET"
+    // }).then(function(data) {
+    //     undateWeatherApp(data);
+    // })
 
-    });
+    let citysList = JSON.parse(localStorage.getItem("cityName"));
+    loadlastCity(citysList)
 }
-
-
+// function to display weather
 function undateWeatherApp(city) {
     console.log(city);
     const imageName = city.weather[0].icon;
@@ -106,17 +89,64 @@ function undateWeatherApp(city) {
     cardInfo.classList.remove("display-none");
 }
 
+// function to call and run forecast
+function forecast(cityid){
+    // *fetch version
+    requestForecastCity(cityid)
+    .then((data) => {
+        displayForecastWeather(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
 
-const requestCity = async (cityName) => {
-    const baseWeatherURL = "https://api.openweathermap.org/data/2.5/weather";
-    const query = `?q=${cityName}&appid=${apiKey}`;
-    // make fetch call (promise call)
-    const response = await fetch(baseWeatherURL + query);
-    // promise data
-    const data = await response.json();
-    return data;
+    // * .ajax version
+    // const queryforecastURL="https://api.openweathermap.org/data/2.5/forecast?id=" + cityid + "&appid=" + apiKey;
+    // $.ajax({
+    //     url:queryforecastURL,
+    //     method:"GET"
+    // }).then((data) => {
+    //      displayForecastWeather(data);
+    // });
 }
 
+// function to display forecast weather
+function displayForecastWeather(response) {
+    for (i=0;i<5;i++){
+        let date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+        let iconcode= response.list[((i+1)*8)-1].weather[0].icon;
+        let iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+        let tempK= response.list[((i+1)*8)-1].main.temp;
+        let tempC=spitOutCelcius(tempK).toFixed(0);
+        let humidity= response.list[((i+1)*8)-1].main.humidity;
+
+        $("#fDate"+i).html(date);
+        $("#fImg"+i).html("<img src="+iconurl+">");
+        $("#fTemp"+i).html(` ${tempC}&deg;C`);
+        $("#fHumidity"+i).html(` ${humidity}%`);
+        cardFInfo.classList.remove("display-none");
+    }
+}
+
+// function to add the search history list button
+function addToList(c) {
+    let listEl = $("<button>"+ c +"</button>");
+    $(listEl).addClass("list-group-item shadow-sm");
+    $(listEl).attr("data-value",c);
+    $(".list-group").append(listEl);
+}
+// function to get the local storage and add button list
+function loadlastCity(){
+    $("ul").empty();
+    let citysList = JSON.parse(localStorage.getItem("cityName"));
+    console.log(citysList);
+    if(citysList!==null){
+        for(i=0; i<citysList.length;i++){
+            addToList(citysList[i]);
+        }
+    }
+}
+// function to run search history
 function PastSearch(event){
     event.preventDefault();
     let liEl = event.target;
@@ -124,16 +154,26 @@ function PastSearch(event){
     if (event.target.matches(".list-group-item")) {
         city = liEl.textContent.trim();
         console.log(city);
-        requestCity(city).then((data) => {
+        // *fetch version
+        requestCity(city)
+        .then((data) => {
             undateWeatherApp(data);
-        }).catch((error) => {
+        })
+        .catch((error) => {
             console.log(error);
         })
-    }
 
+        // *.ajax version
+        // $.ajax({
+        //     url:`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
+        //     method: "GET"
+        // }).then(function(data) {
+        //     undateWeatherApp(data);
+        // })
+    }
 }
 
-
+// function to clear the history div
 function clearHistory(event){
     event.preventDefault();
     citys = [];
@@ -143,20 +183,7 @@ function clearHistory(event){
 }
 
 
-function searchCity(event) {
-    event.preventDefault();
-    const citySearched = cityValue.value.trim();
-    console.log(citySearched);
-    searchForm.reset();
-    requestCity(citySearched).then((data) => {
-        undateWeatherApp(data);
-    }).catch((error) => {
-        console.log(error);
-    })
-    let citysList = JSON.parse(localStorage.getItem("cityName"));
-    loadlastCity(citysList)
-}
-
+// event listener
 $(document).on("click",".list-group-item", PastSearch);
 $(searchForm).on("submit", searchCity);
 $(window).on("load", loadlastCity());
